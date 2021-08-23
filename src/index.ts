b309f0644744a8f6409052720ee990db369913df
Ruslan Zakharov <ruslanzakharov90@gmail.com>
2021-08-23 17:31:28 +0300

    add export Task type

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { RootModule, RootApiProps } from './root/root.module';
-
+import { Task } from  './root/root.api';
 class Gantt {
 	root: RootModule;
+	updateTasks: (tasks: Task[]) => void;
+
 	constructor(el: string, props: RootApiProps) {
 		this.root = new RootModule(el, props);
+		this.updateTasks = this.root.api.updateTasks;
 	}
 }
 
 export default Gantt;
+export { RootApiProps } from  './root/root.module';
+export { Task } from  './root/root.api';
