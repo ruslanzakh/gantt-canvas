7b774f01490d5e77263688ca84c348d902bd638f
Ruslan Zakharov <ruslanzakharov90@gmail.com>
2021-08-29 11:41:44 +0300

    restyle some props

--- src/root/root.api.ts
+++ src/root/root.api.ts
@@ -189,47 +189,47 @@ export class RootApi {
 		this.showMonthMiddle = props.showMonthMiddle ?? false;
 
 		this.monthHeight = props.monthHeight ?? 55;
-		this.renderMonthBottomLine = props.renderMonthBottomLine ?? false;
-		this.renderMonthLeftLine = props.renderMonthLeftLine ?? false;
+		this.renderMonthBottomLine = props.renderMonthBottomLine ?? true;
+		this.renderMonthLeftLine = props.renderMonthLeftLine ?? true;
 		this.monthLineColor = props.monthLineColor ?? '#EAEAEA';
 		this.monthTitleFont = props.monthTitleFont ?? '600 20px Arial';
 		this.monthTitleColor = props.monthTitleColor ?? '#222';
 		this.monthTitleShowYear = props.monthTitleShowYear ?? true;
 
 		this.dayHeight = props.dayHeight ?? 28;
-		this.renderDayStartMonthLine = props.renderDayStartMonthLine ?? false;
+		this.renderDayStartMonthLine = props.renderDayStartMonthLine ?? true;
 		this.dayStartMonthLine = props.dayStartMonthLine ?? '#EAEAEA';
 		this.dayBottomLineColor = props.dayBottomLineColor ?? '#EAEAEA';
-		this.dayTodayBackground = props.dayTodayBackground ?? 'rgba(255,165,0,0.2)';
+		this.dayTodayBackground = props.dayTodayBackground ?? '#f8fdff';
 		this.dayFont = props.dayFont ?? '500 14px Arial';
 		this.dayColor = props.dayColor ?? '#222';
 
-		this.dayColWidth = props.dayColWidth ?? 38;
-		this.rowHeight = props.rowHeight ?? 36;
-		this.colLineColor = props.colLineColor ?? '#EAEAEA';
+		this.dayColWidth = props.dayColWidth ?? 40;
+		this.rowHeight = props.rowHeight ?? 40;
+		this.colLineColor = props.colLineColor ?? '#ebebeb';
 		this.colStartMonthLineColor = props.colStartMonthLineColor;
-		this.rowLineColor = props.rowLineColor ?? '#EAEAEA';
+		this.rowLineColor = props.rowLineColor ?? '#ebebeb';
 		this.rowEvenBackground = props.rowEvenBackground ?? '#fff';
 		this.rowOddBackground = props.rowOddBackground ?? '#fff';
 
-		this.taskDefaultBackground = props.taskDefaultBackground ?? '#F0F0F0';
-		this.taskDefaultHoverBackground = props.taskDefaultHoverBackground ?? '#333333';
+		this.taskDefaultBackground = props.taskDefaultBackground ?? '#ae52d4';
+		this.taskDefaultHoverBackground = props.taskDefaultHoverBackground ?? '#7b1fa2';
 		this.taskDefaultStrokeColor = props.taskDefaultStrokeColor;
 		this.taskDefaultHoverStrokeColor = props.taskDefaultHoverStrokeColor;
-		this.taskDefaultColor = props.taskDefaultColor ?? '#222';
+		this.taskDefaultColor = props.taskDefaultColor ?? '#fff';
 		this.taskDefaultHoverColor = props.taskDefaultHoverColor ?? '#fff';
 		this.taskDefaultOutlineColor = props.taskDefaultOutlineColor ?? '#222';
-		this.taskHeight = props.taskHeight ?? 30;
+		this.taskHeight = props.taskHeight ?? 34;
 		this.taskPadding = props.taskPadding ?? 5;
-		this.taskRadius = props.taskRadius ?? 4;
-		this.taskFont = props.taskFont ?? "14px serif";
+		this.taskRadius = props.taskRadius ?? 2;
+		this.taskFont = props.taskFont ?? "16px serif";
 		this.taskErrorStrokeColor = props.taskErrorStrokeColor;
 		this.minTaskWidth = props.minTaskWidth ?? 25;
 
 		this.taskRenderResizeControls = props.taskRenderResizeControls ?? true;
 		this.taskRenderResizeControlsWidth = props.taskRenderResizeControlsWidth ?? 6;
 		this.taskRenderResizeControlsColor = props.taskRenderResizeControlsColor ?? '#fff';
-		this.taskRenderResizeControlsRadius = props.taskRenderResizeControlsRadius ?? 4;
+		this.taskRenderResizeControlsRadius = props.taskRenderResizeControlsRadius ?? 2;
 
 		this.taskRenderDepControl = props.taskRenderDepControl ?? true;
 		this.taskRenderDepRadius = props.taskRenderDepRadius ?? 7;
@@ -237,9 +237,9 @@ export class RootApi {
 		this.taskRenderDepLineColor = props.taskRenderDepLineColor ?? '#222';
 		this.taskRenderDepBackground = props.taskRenderDepBackground ?? '#fff';
 
-		this.arrowColor = props.arrowColor ?? "#555";
-		this.arrowActiveColor = props.arrowActiveColor ?? "#88BECF";
-		this.arrowRadius = props.arrowRadius ?? 4;
+		this.arrowColor = props.arrowColor ?? "#283593";
+		this.arrowActiveColor = props.arrowActiveColor ?? "#001064";
+		this.arrowRadius = props.arrowRadius ?? 2;
 
 		this.scrollbarXHeight = props.scrollbarXHeight ?? 12;
 		this.scrollbarXBackground = props.scrollbarXBackground ?? '#eee';
